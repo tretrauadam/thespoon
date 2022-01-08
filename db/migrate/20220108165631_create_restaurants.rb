bb4b37be130561e743a09235b39012aace93b489
adamzermane <adamzermane92@yahoo.com>
2022-01-08 18:38:42 +0100

    migrations

---
+++ db/migrate/20220108165631_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :string
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
