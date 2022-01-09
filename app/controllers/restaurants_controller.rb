9f2864a1dff51357dc461b383901f13e790a04b5
adamzermane <adamzermane92@yahoo.com>
2022-01-09 12:22:23 +0100

    restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,12 +2,13 @@
 
 class RestaurantsController < ApplicationController
 
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
 
   end
 
@@ -21,21 +22,26 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restautant = Restaurant.find(params[:id])
+
   end
 
   def update
-    @restautant = Restaurant.find(params[:id])
+
     Restaurant.update(restaurant_params)
     redirect_to restaurant_path
   end
 
   def destroy
-    @restaurant = Restaurant.find(param[:id])
-    
+    @restaurant.destroy
+    redirect_to restaurants_path
+
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def restaurant_params
    params.require(:restaurant).permit(:name, :rating)
