6450b9b1faa569b2d0ff0676a0cf68120a58069f
adamzermane <adamzermane92@yahoo.com>
2022-01-09 12:17:55 +0100

    restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,43 @@
 # frozen_string_literal: true
 
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.all
   end
 
-  def show; end
+  def show
+    @restaurant = Restaurant.find(params[:id])
 
-  def create; end
+  end
 
-  def edit; end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-  def update; end
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to restaurant_path
+  end
 
-  def destroy; end
+  def edit
+    @restautant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restautant = Restaurant.find(params[:id])
+    Restaurant.update(restaurant_params)
+    redirect_to restaurant_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(param[:id])
+    
+  end
+
+  private
+
+  def restaurant_params
+   params.require(:restaurant).permit(:name, :rating)
+  end
 end
